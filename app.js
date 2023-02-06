4066691f03d21017843a03558d699e2bbe3df52f
goodljtnin@gmail.com <goodljtnin@gmail.com>
2023-02-06 23:42:18 +0800

    revise and add seed

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require(`express`)
 const app = express()
+const mongoose = require(`mongoose`)  // 載入 mongoose
+
+if (process.env.NODE_ENV !== `production`) {
+  require(`dotenv`).config()
+}
+mongoose.set(`strictQuery`, false)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })   //設定連線到 mongoDB
+const db = mongoose.connection
+
 app.get(`/`, (req, res) => {
   res.send(`hello~`)
 })
 app.listen(3000, () => {
   console.log(`App is running on port 3000`)
+})
+db.on(`error`, () => {
+  console.log(`mongodb error`)
+})
+db.once(`open`, () => {
+  console.log(`mongodb connected`)
 })
